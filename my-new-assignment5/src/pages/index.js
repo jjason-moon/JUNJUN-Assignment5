1ce0f6a0820fe24f9bb97d2b3692afb887c9d568
jjason-moon <js14769@nyu.edu>
2026-03-22 23:31:29 +0800

    ver1

--- my-new-assignment5/src/pages/index.js
+++ my-new-assignment5/src/pages/index.js
@@ -1,5 +1,25 @@
-import Assignment from './assignment5_JunjunSun'; 
+import Head from "next/head";
+import Assignment from "./assignment5_JunjunSun"; 
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-  return <Assignment />;
+  return (
+    <>
+      <Head>
+        <title>Junjun's Assignment 5</title>
+      </Head>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          
+          {/* 2. 这里是关键！把原来的那一堆 Logo 和链接删掉，换成你的作业 */}
+          <h1 style={{ marginBottom: '20px' }}>My Data Visualization</h1>
+          
+          <div className="assignment-container">
+            <Assignment /> 
+          </div>
+
+        </main>
+      </div>
+    </>
+  );
 }
